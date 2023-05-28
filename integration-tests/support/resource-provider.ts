d3e041aa0ee19545a578eba22b46722c854f5777
Jakub Jirutka <jakub@jirutka.cz>
2023-05-28 22:17:57 +0200

    Replace koa with express in resource-provider
    Express is not better, but oauth2-mock-server depends on express, so
    we can get rid of 33 dependencies.
    
    http-errors is a dependencies of express.

--- integration-tests/support/resource-provider.ts
+++ integration-tests/support/resource-provider.ts
@@ -1,9 +1,8 @@
 import http from 'node:http'
 import { URL } from 'node:url'
 
-import Router from '@koa/router'
+import express, { NextFunction, Request, Response } from 'express'
 import createError from 'http-errors'
-import Koa, { Middleware, Next } from 'koa'
 import LogLevel from 'loglevel'
 
 import { asyncServer, AsyncServer } from './async-server'
@@ -18,64 +17,66 @@ export interface RPOptions {
   clientSecret: string
 }
 
-interface AppContext extends Koa.Context {
-  opts: RPOptions
-}
-
 const log = LogLevel.getLogger('resource-provider')
 
-const oauthAuthenticator = (opts: RPOptions): Middleware<{}, AppContext> => {
+const oauthAuthenticator = (opts: RPOptions) => {
   const url = new URL(opts.introspectionUrl)
   const realm = url == null ? '' : `${url.protocol}//${url.host}`
   const client = createClient()
 
-  return async (ctx: AppContext, next: Next): Promise<void> => {
-    const token = getBearerToken(ctx)
-    if (!token) {
-      ctx.set('WWW-Authenticate', `Bearer realm=${realm}, error=invalid_request`)
-      throw createError(401, 'Unauthenticated')
-    }
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const token = getBearerToken(req)
+      if (!token) {
+        res.setHeader('WWW-Authenticate', `Bearer realm=${realm}, error=invalid_request`)
+        throw createError(401, 'Unauthenticated')
+      }
 
-    log.debug(`Verifying token: ${token}`)
-    const resp = await client.post(opts.introspectionUrl, {
-      form: { token },
-      responseType: 'json',
-      throwHttpErrors: false,
-      username: opts.clientId,
-      password: opts.clientSecret,
-    })
-    const data = resp.body as IntrospectionResponse
+      log.debug(`Verifying token: ${token}`)
+      const resp = await client.post(opts.introspectionUrl, {
+        form: { token },
+        responseType: 'json',
+        throwHttpErrors: false,
+        username: opts.clientId,
+        password: opts.clientSecret,
+      })
+      const data = resp.body as IntrospectionResponse
 
-    if (!data.active) {
-      ctx.set('WWW-Authenticate', `Bearer realm=${realm}, error=invalid_token`)
-      throw createError(401, 'Invalid access token')
-    }
-    if (resp.statusCode !== 200 || !('client_id' in data)) {
-      throw createError(503, 'Unable to verify access token')
-    }
+      if (!data.active) {
+        res.setHeader('WWW-Authenticate', `Bearer realm=${realm}, error=invalid_token`)
+        throw createError(401, 'Invalid access token')
+      }
+      if (resp.statusCode !== 200 || !('client_id' in data)) {
+        throw createError(503, 'Unable to verify access token')
+      }
 
-    log.debug('Client provided a valid token')
-    await next()
+      log.debug('Client provided a valid token')
+      next()
+
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
-async function errorMiddleware (ctx: AppContext, next: Next): Promise<any> {
-  try {
-    await next()
-  } catch (err: any) {
-    log.warn(err)
+function errorHandler (err: any, _req: Request, res: Response, next: NextFunction) {
+  log.warn(err)
 
-    // Tell Koa's onerror handler to not log this error.
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    err.expose = true
-
-    ctx.app.emit('error', err, ctx)
+  if (res.headersSent) {
+    return next(err)
   }
+  const status = err.statusCode ?? 500
+  const message = err.message ?? 'Server Error'
+
+  res.status(status).json({
+    status,
+    message,
+  })
 }
 
-function getBearerToken (ctx: AppContext): string | undefined {
+function getBearerToken (req: Request): string | undefined {
   // Parse access token from the Authorization header
-  const authorization = ctx.get('authorization')
+  const authorization = req.get('authorization')
   if (authorization?.startsWith('Bearer ')) {
     return authorization.split(' ', 2)[1]
   }
@@ -83,25 +84,19 @@ function getBearerToken (ctx: AppContext): string | undefined {
 }
 
 function createApp (opts: RPOptions) {
-  const router = new Router<void, {}>()
-    .get('/', ctx => {
-      ctx.status = 204
+  const app = express()
+    .use(express.json())
+    .get('/', (_req, res) => {
+      res.sendStatus(204)
     })
-    .get('/secured/ping', oauthAuthenticator(opts), async ctx => {
-      ctx.body = 'pong'
-      ctx.status = 200
+    .get('/secured/ping', oauthAuthenticator(opts), (_req, res) => {
+      res.send('pong')
     })
-
-  const app = new Koa()
-    .use(errorMiddleware)
-    .use(router.routes())
-    .use(router.allowedMethods())
-
-  app.context.opts = opts
+    .use(errorHandler)
 
   return app
 }
 
 export function createServer (opts: RPOptions): AsyncServer {
-  return asyncServer(http.createServer(createApp(opts).callback()))
+  return asyncServer(http.createServer(createApp(opts)))
 }
