e10da92eb5f2a580a1d0f5acde82f90537da0f4e
Jakub Jirutka <jakub@jirutka.cz>
2023-06-05 00:50:03 +0200

    Read config options lazily

--- src/config-reader.ts
+++ src/config-reader.ts
@@ -37,13 +37,13 @@ export type DeriveConfigType <T extends ConfigDescriptor> = {
 export type ConfigReader<T> = (variables: Record<string, string | undefined>) => T
 
 /**
- * Creates a function that reads the specified configuration options from nginx
- * variables.
+ * Creates a function that reads the specified configuration options from the
+ * nginx variables.
  *
  * Option names are expected to be in camelCase notation and corresponding nginx
  * variables in snake_case notation with the specified prefix. For example, if
- * `varPrefix` is `'oauth_'`, then option `clientSecret` is read from nginx variable
- * `oauth_client_secret`.
+ * `varPrefix` is `'oauth_'`, then option `clientSecret` is read from nginx
+ * variable `oauth_client_secret`.
  *
  * @param configDescriptor An object with all the configuration options and their
  *   default values or `undefined` in the case of required variables.
@@ -53,25 +53,37 @@ export const createConfigReader = <T extends ConfigDescriptor> (
   configDescriptor: T,
   varPrefix: string,
 ): ConfigReader<DeriveConfigType<T>> => (variables) => {
+  const config = Object.create(null)
 
-  return Object.keys(configDescriptor).reduce<Record<string, unknown>>((config, key) => {
-    const varName = varPrefix + camelToSnake(key)
-    const rawValue = variables[varName]
-    const desc = configDescriptor[key]
+  for (const key of Object.keys(configDescriptor)) {
+    // Use lazy-loading.
+    Object.defineProperty(config, key, {
+      configurable: true,
+      enumerable: true,
+      get () {
+        const varName = varPrefix + camelToSnake(key as string)
+        const rawValue = variables[varName]
+        const desc = configDescriptor[key]
 
-    let value: unknown
-    if (rawValue != null && rawValue !== '') {
-      value = isOptionDescriptor(desc) ? desc.parser(rawValue) : parseFromStr(rawValue, typeof desc)
-    } else {
-      value = isOptionDescriptor(desc) ? desc.default : desc
-    }
-    if (value === undefined) {
-      throw Error(`Required variable $${varName} is not set`)
-    }
-    config[key] = value
+        let value: unknown
+        if (rawValue != null && rawValue !== '') {
+          value = isOptionDescriptor(desc) ? desc.parser(rawValue) : parseFromStr(rawValue, typeof desc)
+        } else {
+          value = isOptionDescriptor(desc) ? desc.default : desc
+        }
+        if (value === undefined) {
+          throw Error(`Required variable $${varName} is not set`)
+        }
 
-    return config
-  }, Object.create(null)) as DeriveConfigType<T>
+        // Replace property with a plain value assignment.
+        delete config[key]
+        config[key] = value
+
+        return value
+      }
+    })
+  }
+  return config
 }
 
 function isOptionDescriptor (value: any): value is OptionDescriptor {
